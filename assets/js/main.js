669364b5421079ce3c599d18101b5b5b9cb348f1
ricardo <ricardolimadelemos.trab@gmail.com>
2022-08-27 12:39:11 -0300

    Files

---
+++ assets/js/main.js
@@ -0,0 +1,12 @@
+
+//interação com o formulário
+const txtName = document.getElementById('txtName')
+const txtEmail = document.getElementById('txtEmail')
+const txtLevel = document.getElementById('txtLevel')
+const txtCharacter = document.getElementById('txtCharacter')
+
+const btnSubscribe = document.getElementById(`btnSubscribe`)
+
+btnSubscribe.addEventListener('click', () => {
+  
+}  )
